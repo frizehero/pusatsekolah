ce87a06f0a3eb8f2a16a6edd7ac60a88057bcdf3
RoisAntono <67670208+RoisAntono@users.noreply.github.com>
2021-03-26 13:07:51 +0700

    update

--- pusatsekolah/application/modules/template/views/view_template_core.php
+++ pusatsekolah/application/modules/template/views/view_template_core.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="Content-Language" content="en">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>CORE HMVC 3 </title>
+    <title>Pusat Sekolah</title>
     <meta name="viewport
         content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
     <meta name="description" content="Rumah Sakit Terbaik Di Pasuruan.">
@@ -16,7 +16,9 @@
     <!-- Disable tap highlight on IE -->
     <meta name="msapplication-tap-highlight" content="no">
 
-<link href="<?php echo base_url() ?>main.css" rel="stylesheet"></head>
+<link href="<?php echo base_url() ?>main.css" rel="stylesheet">
+<link rel="shortcut icon" href="assets/images/logo.ico">
+</head>
 
 <style type="text/css">
     .modal {
