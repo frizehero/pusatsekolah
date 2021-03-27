d94a145928700754d9f56e31eb3843051d974449
Dhinda_Mak <dhiindarj311@gmail.com>
2021-03-27 08:50:49 +0700

    tampilan prestasi

--- pusatsekolah/application/modules/prestasi/models/M_beranda.php
+++ pusatsekolah/application/modules/prestasi/models/M_beranda.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class M_beranda extends CI_Model {
+class M_beranda extends CI_Model
+{
 
 	function tampil()
 	{
@@ -15,40 +16,36 @@ class M_beranda extends CI_Model {
 
 
 		$this->load->library('upload');
-		$nmfile = "file_".time();
+		$nmfile = "file_" . time();
 		$config['upload_path']		= 'assets/img/';
 		$config['allowed_types']	= 'gif|jpg|png|jpeg';
 		$config['max_size']			= 5120;
 		$config['max_width']		= 4300;
 		$config['max_height']		= 4300;
 		$config['file_name'] 		= $nmfile;
-		
+
 		$this->upload->initialize($config);
-		
-		if($_FILES['gambar']['name'])
-        {
-            if ($this->upload->do_upload('gambar'))
-            {
+
+		if ($_FILES['gambar']['name']) {
+			if ($this->upload->do_upload('gambar')) {
 				$gbr = $this->upload->data();
 				$data = array(
 					'nama_sekolah'		=> $nama,
 					'keterangan'		=> $keterangan,
 					'logo' 				=> $gbr['file_name'],
-					
-					
-				);
-				$this->db->insert('sekolah', $data);
-			
-			}	 
-		}
-		else{
-				$data = array(
-					'nama_sekolah'		=> $nama,
-					'keterangan'		=> $keterangan,
-					'logo' 				=> 'kosong1.png',
+
+
 				);
 				$this->db->insert('sekolah', $data);
 			}
+		} else {
+			$data = array(
+				'nama_sekolah'		=> $nama,
+				'keterangan'		=> $keterangan,
+				'logo' 				=> 'kosong1.png',
+			);
+			$this->db->insert('sekolah', $data);
+		}
 	}
 
 	function edit()
@@ -59,37 +56,33 @@ class M_beranda extends CI_Model {
 
 
 		$this->load->library('upload');
-		$nmfile = "file_".time();
+		$nmfile = "file_" . time();
 		$config['upload_path']		= 'assets/img/';
 		$config['allowed_types']	= 'gif|jpg|png|jpeg';
 		$config['max_size']			= 5120;
 		$config['max_width']		= 4300;
 		$config['max_height']		= 4300;
 		$config['file_name'] 		= $nmfile;
-		
+
 		$this->upload->initialize($config);
-		
-		if($_FILES['gambar']['name'])
-        {
-            if ($this->upload->do_upload('gambar'))
-            {
+
+		if ($_FILES['gambar']['name']) {
+			if ($this->upload->do_upload('gambar')) {
 				$gbr = $this->upload->data();
 				$data = array(
 					'nama_sekolah'		=> $nama,
 					'keterangan'		=> $keterangan,
 					'logo' 				=> $gbr['file_name'],
 				);
-				$this->db->where('id_sekolah',$id_sekolah)->update('sekolah', $data);
-			
-			}	 
-		}
-		else{
-				$data = array(
-					'nama_sekolah'		=> $nama,
-					'keterangan'		=> $keterangan,
-				);
-				$this->db->where('id_sekolah',$id_sekolah)->update('sekolah', $data);
+				$this->db->where('id_sekolah', $id_sekolah)->update('sekolah', $data);
 			}
+		} else {
+			$data = array(
+				'nama_sekolah'		=> $nama,
+				'keterangan'		=> $keterangan,
+			);
+			$this->db->where('id_sekolah', $id_sekolah)->update('sekolah', $data);
+		}
 	}
 
 	function hapus($id)
@@ -100,6 +93,6 @@ class M_beranda extends CI_Model {
 	function cari()
 	{
 		$cari 		= $this->input->post('cari');
-		return $this->db->like('nama_sekolah',$cari)->get('sekolah')->result();
+		return $this->db->like('nama_sekolah', $cari)->get('sekolah')->result();
 	}
 }
