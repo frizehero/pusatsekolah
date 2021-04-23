30db3896fb877c093b27783719b935d9c99c6498
Dhinda_Mak <dhiindarj311@gmail.com>
2021-04-23 19:16:05 +0700

    data guru

--- pusatsekolah/application/modules/data_guru/views/V_data_guru.php
+++ pusatsekolah/application/modules/data_guru/views/V_data_guru.php
@@ -93,6 +93,7 @@
     <?php tampilnotif() ?>
     <div class="row">
         <?php $no = 1;
+        foreach ($tampil as $rowP) { ?>
             <div class="col-md-12 col-lg-6 col-xl-4">
                 <div class="card-shadow-primary card-border mb-3 card">
                     <div class="dropdown-menu-header">
