22001a033daf89cd11029d8c2fcea861a3d9629d
Dhinda_Mak <dhiindarj311@gmail.com>
2021-05-24 11:47:48 +0700

    Tampilan Alumni user (user)

---
+++ userpusatsekolah/application/modules/tampilan_alumniuser/views/V_alumniuser.php
@@ -0,0 +1,187 @@
+<div class="content">
+    <!--  section  -->
+    <section class="parallax-section dashboard-header-sec gradient-bg" data-scrollax-parent="true">
+        <div class="container">
+            <!--<div class="dashboard-breadcrumbs breadcrumbs"><a href="#">Home</a><a href="#">Edit Profil</a><span>Main page</span></div>-->
+            <!--Tariff Plan menu-->
+            <div class="tfp-btn"><span>Beralih Ke : </span>
+                <a href="tampilan-beranda-user.html"><strong>Tampilan Sekolah</strong></a>
+            </div>
+            <!--Tariff Plan menu end-->
+            <div class="dashboard-header_conatiner fl-wrap dashboard-header_title">
+                <h1><span>Emak Sukses</span></h1>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="dashboard-header fl-wrap" style="height: 80px;">
+            <div class="container">
+                <div class="dashboard-header_conatiner fl-wrap">
+                    <div class="dashboard-header-avatar">
+                        <img src="images/avatar/4.jpg" alt="">
+                        <a href="tampilan-edit-profiluser.html" class="color-bg edit-prof_btn"><i class="fal fa-edit"></i></a>
+                    </div>
+                    <br><br>
+                </div>
+            </div>
+        </div>
+        <div class="gradient-bg-figure" style="right:-30px;top:10px;"></div>
+        <div class="gradient-bg-figure" style="left:-20px;bottom:30px;"></div>
+        <div class="circle-wrap" style="left:120px;bottom:120px;" data-scrollax="properties: { translateY: '-200px' }">
+            <div class="circle_bg-bal circle_bg-bal_small"></div>
+        </div>
+        <div class="circle-wrap" style="right:420px;bottom:-70px;" data-scrollax="properties: { translateY: '150px' }">
+            <div class="circle_bg-bal circle_bg-bal_big"></div>
+        </div>
+        <div class="circle-wrap" style="left:420px;top:-70px;" data-scrollax="properties: { translateY: '100px' }">
+            <div class="circle_bg-bal circle_bg-bal_big"></div>
+        </div>
+        <div class="circle-wrap" style="left:40%;bottom:-70px;">
+            <div class="circle_bg-bal circle_bg-bal_middle"></div>
+        </div>
+        <div class="circle-wrap" style="right:40%;top:-10px;">
+            <div class="circle_bg-bal circle_bg-bal_versmall" data-scrollax="properties: { translateY: '-350px' }"></div>
+        </div>
+        <div class="circle-wrap" style="right:55%;top:90px;">
+            <div class="circle_bg-bal circle_bg-bal_versmall" data-scrollax="properties: { translateY: '-350px' }"></div>
+        </div>
+    </section>
+    <!--  section  end-->
+    <!--  section  -->
+    <section class="gray-bg main-dashboard-sec" id="sec1">
+        <div class="container">
+            <!--  dashboard-menu-->
+            <div class="col-md-3">
+                <div class="mob-nav-content-btn color2-bg init-dsmen fl-wrap"><i class="fal fa-bars"></i> Dashboard menu</div>
+                <div class="clearfix"></div>
+                <div class="fixed-bar fl-wrap" id="dash_menu">
+                    <div class="user-profile-menu-wrap fl-wrap block_box">
+                        <!-- user-profile-menu-->
+                        <div class="user-profile-menu">
+                            <h3>AKUN SAYA</h3>
+                            <ul class="no-list-style">
+                                <li><a href="tampilan-inbox-user.html"><i class="fal fa-envelope"></i> Inbox<span>3</span></a></li>
+                                <li><a href="tampilan-favoritsaya.html"><i class="fal fa-heart"></i>Favorit Saya</a></li>
+                                <li><a href="tampilan-terakhirdilihat-user.html"><i class="fal fa-dot-circle"></i>Terakhir Dilihat <span>7</span></a></li>
+                                <li><a href="tampilan-hasilreport-user.html"><i class="fal fa-times-circle"></i> Hasil Report</a></li>
+                                <li><a href="tampilan-edit-profiluser.html"><i class="fal fa-user-edit"></i> Edit profil</a></li>
+                                <li><a href="tampilan-alumni-user.html" class="user-profile-act"><i class="fal fa-layer-plus"></i>Alumni</a></li>
+                                <li><a href="tampilan-undang-teman.html"><i class="fal fa-user-plus"></i>Undang Teman</a></li>
+                                <li><a href="tampilan-ubah-password.html"><i class="fal fa-key"></i>Ubah Password</a></li>
+                            </ul>
+                        </div>
+                        <!-- user-profile-menu end-->
+                        <!-- user-profile-menu-->
+                        <div class="user-profile-menu">
+                            <h3>BANTUAN</h3>
+                            <ul class="no-list-style">
+                                <li><a href="tampilan-pusatbantuan.html"><i class="fal fa-th-list"></i> Pusat Bantuan </a></li>
+                                <li><a href="tampilan-nilaips-user.html"><i class="fal fa-comments-alt"></i> Suka Pusat Sekolah? Nilai Kami! </a></li>
+                                <li><a href="tampilan-hapusakun-user.html"><i class="fal fa-edit"></i> Ajukan Penghapusan Akun</a></li>
+                                <li><a href="tampilan-chatps-user.html"><i class="fal fa-envelope"></i> Chat Dengan Pusat Sekolah <span>3</span></a></li>
+                            </ul>
+                        </div>
+                        <!-- user-profile-menu end-->
+                        <button class="logout_btn color2-bg">Log Out <i class="fas fa-sign-out"></i></button>
+                    </div>
+                </div>
+                <a class="back-tofilters color2-bg custom-scroll-link fl-wrap" href="#dash_menu">Tampilan Profil<i class="fas fa-caret-up"></i></a>
+                <div class="clearfix"></div>
+            </div>
+            <!-- dashboard-menu  end-->
+            <!-- dashboard content-->
+            <div class="col-md-9">
+                <div class="dashboard-title   fl-wrap">
+                    <h3>Kamu Telah Mendaftar Sebagai Alumni</h3>
+                </div>
+                <!-- profile-edit-container-->
+                <div class="accordion mar-top" id="sec3">
+                    <a class="toggle act-accordion" href="#"> PAUD Bhayangkara 205 - Lulusan 2000<span></span></a>
+                    <div class="accordion-inner visible" style="display: none;">
+                        <div class="list-single-main-item_content fl-wrap">
+                            <div class="rooms-container fl-wrap">
+                                <div class="rooms-item fl-wrap">
+                                    <div class="rooms-media">
+                                        <img src="images/sekolah/paud2.jpg" alt="" style="height: 185px;">
+                                    </div>
+                                    <div class="rooms-details">
+                                        <div class="rooms-details-header fl-wrap">
+                                            <h3>PAUD Bhayangkara 205</h3>
+                                            <h5>Tahun Lulus : <span> 2000</span></h5>
+                                        </div>
+                                        <p>Morbi varius, nulla sit amet rutrum elementum, est elit finibus tellus, ut tristique elit risus at metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in pulvinar neque. Nulla finibus lobortis pulvinar. Donec a consectetur nulla. Nulla posuere sapien vitae </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-3"></div>
+                                <div class="col-md-3"></div>
+                                <div class="col-md-3"></div>
+                                <div class="col-md-3">
+                                    <button class="logout_btn color2-bg">Hapus <i class="fas fa-sign-out"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="toggle" href="#"> TK Barnadian Uta - Lulusan 2002 <span></span></a>
+                    <div class="accordion-inner" style="display: none;">
+                        <div class="list-single-main-item_content fl-wrap">
+                            <div class="rooms-container fl-wrap">
+                                <div class="rooms-item fl-wrap">
+                                    <div class="rooms-media">
+                                        <img src="images/sekolah/tk1.jpg" alt="" style="height: 185px;">
+                                    </div>
+                                    <div class="rooms-details">
+                                        <div class="rooms-details-header fl-wrap">
+                                            <h3>TK Barnadian Uta</h3>
+                                            <h5>Tahun Lulus : <span> 2002</span></h5>
+                                        </div>
+                                        <p>Morbi varius, nulla sit amet rutrum elementum, est elit finibus tellus, ut tristique elit risus at metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in pulvinar neque. Nulla finibus lobortis pulvinar. Donec a consectetur nulla. Nulla posuere sapien vitae </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-3"></div>
+                                <div class="col-md-3"></div>
+                                <div class="col-md-3"></div>
+                                <div class="col-md-3">
+                                    <button class="logout_btn color2-bg">Hapus <i class="fas fa-sign-out"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="toggle" href="#"> SDN Ardimulyo 1 - Lulusan 2008 <span></span></a>
+                    <div class="accordion-inner" style="">
+                        <div class="list-single-main-item_content fl-wrap">
+                            <div class="rooms-container fl-wrap">
+                                <div class="rooms-item fl-wrap">
+                                    <div class="rooms-media">
+                                        <img src="images/sekolah/sd1.jpg" alt="" style="height: 185px;">
+                                    </div>
+                                    <div class="rooms-details">
+                                        <div class="rooms-details-header fl-wrap">
+                                            <h3>SDN Ardimulyo 1</h3>
+                                            <h5>Tahun Lulus : <span> 2008</span></h5>
+                                        </div>
+                                        <p>Morbi varius, nulla sit amet rutrum elementum, est elit finibus tellus, ut tristique elit risus at metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in pulvinar neque. Nulla finibus lobortis pulvinar. Donec a consectetur nulla. Nulla posuere sapien vitae </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-3"></div>
+                                <div class="col-md-3"></div>
+                                <div class="col-md-3"></div>
+                                <div class="col-md-3">
+                                    <button class="logout_btn color2-bg">Hapus <i class="fas fa-sign-out"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- profile-edit-container end-->
+            </div>
+            <!-- dashboard content end-->
+        </div>
+    </section>
+    <!--  section  end-->
+    <div class="limit-box fl-wrap"></div>
+</div>
