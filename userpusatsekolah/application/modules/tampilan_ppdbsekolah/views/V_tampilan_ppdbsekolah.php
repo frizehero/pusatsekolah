575310c5618ec457c31e22f6abe388488008b8be
Dhinda_Mak <dhiindarj311@gmail.com>
2021-05-24 14:21:56 +0700

    Tampilan PPDB sekolah-sekolah (user)

---
+++ userpusatsekolah/application/modules/tampilan_ppdbsekolah/views/V_tampilan_ppdbsekolah.php
@@ -0,0 +1,229 @@
+<div class="content">
+    <!--  section  -->
+    <section class="parallax-section single-par" data-scrollax-parent="true">
+        <div class="bg par-elem " data-bg="images/sekolah/ppdb1.jpg" data-scrollax="properties: { translateY: '30%' }"></div>
+        <div class="overlay op7"></div>
+        <div class="container">
+            <div class="section-title center-align big-title">
+                <h2><span>PPDB Sekolah</span></h2>
+                <span class="section-separator"></span>
+                <h4>Dapatkan kumpulan informasi PPDB di Pusat Sekolah agar kamu lebih mudah menemukan informasi penerimaan sekolah baru yang kamu cari.</h4>
+            </div>
+        </div>
+        <div class="header-sec-link">
+            <a href="#sec1" class="custom-scroll-link"><i class="fal fa-angle-double-down"></i></a>
+        </div>
+    </section>
+    <!--  section  end-->
+    <section class="gray-bg no-top-padding-sec" id="sec1">
+        <div class="container">
+            <div class="breadcrumbs inline-breadcrumbs fl-wrap block-breadcrumbs">
+                <a href="#">Home</a><a href="#">Beranda</a> <span>PPDB</span>
+                <div class="showshare brd-show-share color2-bg"> <i class="fas fa-share"></i> Bagikan </div>
+            </div>
+            <div class="share-holder hid-share sing-page-share top_sing-page-share">
+                <div class="share-container  isShare"></div>
+            </div>
+            <div class="post-container fl-wrap">
+                <div class="box-widget-wrap fl-wrap">
+                    <div class="box-widget-item fl-wrap block_box">
+                        <div class="box-widget-item-header">
+                            <h3>Cari Sekolah</h3>
+                        </div>
+                        <div class="box-widget fl-wrap">
+                            <div class="box-widget-content">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="search-widget">
+                                            <form action="#" class="fl-wrap">
+                                                <input name="se" id="se" type="text" class="search" placeholder="Cari ..." value="" />
+                                                <button class="search-submit color2-bg" id="submit_btn"><i class="fal fa-search"></i> </button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="listsearch-input-item">
+                                            <select data-placeholder="Location" class="chosen-select no-search-select">
+                                                <option>Semua Jenjang</option>
+                                                <option>PAUD</option>
+                                                <option>TK</option>
+                                                <option>SD</option>
+                                                <option>SMP</option>
+                                                <option>SMA</option>
+                                                <option>SMK</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="listsearch-input-item">
+                                            <select data-placeholder="City/Location" class="chosen-select no-search-select">
+                                                <option>Jakarta</option>
+                                                <option>Surabaya</option>
+                                                <option>Balikpapan</option>
+                                                <option>Bandung</option>
+                                                <option>Jogja</option>
+                                                <option>Jepara</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <!-- blog content-->
+                    <div class="col-md-8">
+                        <!-- article> -->
+                        <div class="list-single-main-item fl-wrap block_box" id="sec4">
+                            <div class="list-single-main-item-title">
+                                <h3>Informasi PPDB Terbaru</h3>
+                            </div>
+                            <div class="list-single-main-item_content fl-wrap">
+                                <!--   rooms-container -->
+                                <div class="rooms-container fl-wrap">
+                                    <!--  rooms-item -->
+                                    <div class="rooms-item fl-wrap">
+                                        <div class="rooms-media">
+                                            <img src="images/Sekolah/ppdb1.jpg" alt="">
+                                        </div>
+                                        <div class="rooms-details">
+                                            <div class="rooms-details-header fl-wrap">
+                                                <div class="row">
+                                                    <div class="col-md-10">
+                                                        <a href="tampilan-beranda-sekolah.html">
+                                                            <h3>Penerimaan Siswa Didik Baru SMK Negeri 1 Kota Probolinggo</h3>
+                                                        </a>
+                                                    </div>
+                                                    <div class="col-md-2">
+                                                        <div class="show-more-snopt"><i class="fal fa-ellipsis-h"></i></div>
+                                                        <div class="show-more-snopt-tooltip">
+                                                            <a href="tampilan-beranda-sekolah.html"> <i class="fas fa-comment-alt"></i> Lihat Lengkap</a>
+                                                            <a href="#"> <i class="fas fa-flag-alt"></i> Report</a>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-10">
+                                                        <h5>20 Nov 2020<span> - 3 Hari Lagi</span></h5>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <p>Morbi varius, nulla sit amet rutrum elementum, est elit finibus tellus, ut tristique elit risus at metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in pulvinar neque. Nulla finibus lobortis pulvinar. Donec a consectetur nulla. Nulla posuere sapien vitae </p>
+                                        </div>
+                                    </div>
+                                    <div class="rooms-item fl-wrap">
+                                        <div class="rooms-media">
+                                            <img src="images/Sekolah/ppdb1.jpg" alt="">
+                                        </div>
+                                        <div class="rooms-details">
+                                            <div class="rooms-details-header fl-wrap">
+                                                <div class="row">
+                                                    <div class="col-md-10">
+                                                        <a href="tampilan-beranda-sekolah.html">
+                                                            <h3>Penerimaan Siswa Didik Baru SMK Negeri 1 Kota Probolinggo</h3>
+                                                        </a>
+                                                    </div>
+                                                    <div class="col-md-2">
+                                                        <div class="show-more-snopt"><i class="fal fa-ellipsis-h"></i></div>
+                                                        <div class="show-more-snopt-tooltip">
+                                                            <a href="tampilan-beranda-sekolah.html"> <i class="fas fa-comment-alt"></i> Lihat Lengkap</a>
+                                                            <a href="#"> <i class="fas fa-flag-alt"></i> Report</a>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-10">
+                                                        <h5>20 Nov 2020<span> - 3 Hari Lagi</span></h5>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <p>Morbi varius, nulla sit amet rutrum elementum, est elit finibus tellus, ut tristique elit risus at metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in pulvinar neque. Nulla finibus lobortis pulvinar. Donec a consectetur nulla. Nulla posuere sapien vitae </p>
+                                        </div>
+                                    </div>
+                                    <div class="rooms-item fl-wrap">
+                                        <div class="rooms-media">
+                                            <img src="images/Sekolah/ppdb1.jpg" alt="">
+                                        </div>
+                                        <div class="rooms-details">
+                                            <div class="rooms-details-header fl-wrap">
+                                                <div class="row">
+                                                    <div class="col-md-10">
+                                                        <a href="tampilan-beranda-sekolah.html">
+                                                            <h3>Penerimaan Siswa Didik Baru SMK Negeri 1 Kota Probolinggo</h3>
+                                                        </a>
+                                                    </div>
+                                                    <div class="col-md-2">
+                                                        <div class="show-more-snopt"><i class="fal fa-ellipsis-h"></i></div>
+                                                        <div class="show-more-snopt-tooltip">
+                                                            <a href="tampilan-beranda-sekolah.html"> <i class="fas fa-comment-alt"></i> Lihat Lengkap</a>
+                                                            <a href="#"> <i class="fas fa-flag-alt"></i> Report</a>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-10">
+                                                        <h5>20 Nov 2020<span> - 3 Hari Lagi</span></h5>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <p>Morbi varius, nulla sit amet rutrum elementum, est elit finibus tellus, ut tristique elit risus at metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in pulvinar neque. Nulla finibus lobortis pulvinar. Donec a consectetur nulla. Nulla posuere sapien vitae </p>
+                                        </div>
+                                    </div>
+                                    <!--  rooms-item end -->
+                                </div>
+                                <!--   rooms-container end -->
+                            </div>
+                        </div>
+                        <!-- article end -->
+                        <div class="pagination">
+                            <a href="#" class="prevposts-link"><i class="fas fa-caret-left"></i><span>Prev</span></a>
+                            <a href="#">1</a>
+                            <a href="#" class="current-page">2</a>
+                            <a href="#">3</a>
+                            <a href="#">...</a>
+                            <a href="#">7</a>
+                            <a href="#" class="nextposts-link"><span>Next</span><i class="fas fa-caret-right"></i></a>
+                        </div>
+                    </div>
+                    <!-- blog conten end-->
+                    <!-- blog sidebar -->
+                    <div class="col-md-4">
+                        <div class="box-widget-wrap fl-wrap fixed-bar">
+                            <div class="box-widget-item fl-wrap block_box">
+                                <div class="box-widget-item-header">
+                                    <h3>Tagar</h3>
+                                </div>
+                                <div class="box-widget fl-wrap">
+                                    <div class="box-widget-content">
+                                        <div class="list-single-tags tags-stylwrap">
+                                            <a href="#">#PPDB2020</a>
+                                            <a href="#">#PPDB-Swasta2020</a>
+                                            <a href="#">#PPDB-SD2020</a>
+                                            <a href="#">#PPDB-TK2020</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--box-widget-item end -->
+                            <!--box-widget-item -->
+                            <div class="box-widget-item fl-wrap block_box">
+                                <div class="box-widget-item-header">
+                                    <h3>Kategori</h3>
+                                </div>
+                                <div class="box-widget fl-wrap">
+                                    <div class="box-widget-content">
+                                        <ul class="cat-item no-list-style">
+                                            <li><a href="#">Terbaru</a> <span>3</span></li>
+                                            <li><a href="#">SMK</a> <span>6 </span></li>
+                                            <li><a href="#">SD</a> <span>12 </span></li>
+                                            <li><a href="#">TK</a> <span>4</span></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--box-widget-item end -->
+                        </div>
+                    </div>
+                    <!-- blog sidebar end -->
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="limit-box fl-wrap"></div>
+</div>
