ca7e6f64eeffe68fcc87ef716bdb7901604e7f79
RoisAntono <67670208+RoisAntono@users.noreply.github.com>
2021-05-24 15:43:40 +0700

    update footer

--- userpusatsekolah/application/modules/beranda_user/views/V_beranda_user.php
+++ userpusatsekolah/application/modules/beranda_user/views/V_beranda_user.php
@@ -1289,3 +1289,126 @@
     </section>
     <!--section end-->
 </div>
+<!--footer -->
+<footer class="main-footer fl-wrap">
+    <!-- footer-header-->
+    <div class="footer-header fl-wrap grad ient-dark">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                    <div  class="subscribe-header">
+                        <h3>Subscribe Pusat Sekolah untuk <span>notifikasi terbaru</span></h3>
+                        <p>Kamu hanya perlu menuliskan alamat emailmu</p>
+                    </div>
+                </div>
+                <div class="col-md-7">
+                    <div class="subscribe-widget">
+                        <div class="subcribe-form">
+                            <form id="subscribe">
+                                <input class="enteremail fl-wrap" name="email" id="subscribe-email" placeholder="Tulis Email Anda ..." spellcheck="false" type="text">
+                                <button type="submit" id="subscribe-button" class="subscribe-button"><i class="fal fa-envelope"></i></button>
+                                <label for="subscribe-email" class="subscribe-message"></label>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- footer-header end-->
+    <!--footer-inner-->
+    <div class="footer-inner   fl-wrap">
+        <div class="container">
+            <div class="row">
+                <!-- footer-widget-->
+                <div class="col-md-4">
+                    <div class="footer-widget fl-wrap">
+                        <div class="footer-logo"><a href="index.html"><img src="images/logoatas.png" alt=""></a></div>
+                        <div class="footer-contacts-widget fl-wrap">
+                            <p>Pusat Sekolah hadir sebagai salah satu sara mudah bagi sekolah - sekolah di selurh Indonesia agar saling terhubung dan mudah dalam menyampaikan informasi tentang sekolah mereka.   </p>
+                            <ul  class="footer-contacts fl-wrap no-list-style">
+                                <li><span><i class="fal fa-envelope"></i> Mail :</span><a href="#" target="_blank">cp@pusatsekolah.com</a></li>
+                                <li> <span><i class="fal fa-map-marker"></i> Adress :</span><a href="#" target="_blank">Ngijo, Malang, Jawa Timur, Indonesia</a></li>
+                                <li><span><i class="fal fa-phone"></i> Phone :</span><a href="#">(+62)8213567331</a></li>
+                            </ul>
+                            <div class="footer-social">
+                                <span>Find  us on: </span>
+                                <ul class="no-list-style">
+                                    <li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                                    <li><a href="#" target="_blank"><i class="fab fa-whatsapp"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- footer-widget end-->
+                <!-- footer-widget-->
+                <div class="col-md-4">
+                    <div class="footer-widget fl-wrap">
+                        <h3>Event Terbaru</h3>
+                        <div class="footer-widget-posts fl-wrap">
+                            <ul class="no-list-style">
+                                <li class="clearfix">
+                                    <a href="#"  class="widget-posts-img"><img src="images/all/4.jpg" class="respimg" alt=""></a>
+                                    <div class="widget-posts-descr">
+                                        <a href="#" title="">SMK Negeri 1 Probolinggo Rayakan Dies Natalis Ke-98</a>
+                                        <span class="widget-posts-date"><i class="fal fa-calendar"></i> 21 Mar 09.05 </span> 
+                                    </div>
+                                </li>
+                                <li class="clearfix">
+                                    <a href="#"  class="widget-posts-img"><img src="images/all/2.jpg" class="respimg" alt=""></a>
+                                    <div class="widget-posts-descr">
+                                        <a href="#" title="">Pusat Sekolah Berkunjung ke Istana Presiden</a>
+                                        <span class="widget-posts-date"><i class="fal fa-calendar"></i> 7 Mar 18.21 </span> 
+                                    </div>
+                                </li>
+                                <li class="clearfix">
+                                    <a href="#"  class="widget-posts-img"><img src="images/all/7.jpg" class="respimg" alt=""></a>
+                                    <div class="widget-posts-descr">
+                                        <a href="#" title="">SMPN 10 Karanganyar Mulai Buka PPDB 2021</a>
+                                        <span class="widget-posts-date"><i class="fal fa-calendar"></i> 7 Mar 16.42 </span>
+                                    </div>
+                                </li>
+                            </ul>
+                            <a href="tampilan-event-sekolah.html" class="footer-link">Baca lebih banyak <i class="fal fa-long-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <!-- footer-widget end-->
+                <!-- footer-widget  -->
+                <div class="col-md-4">
+                    <div class="footer-widget fl-wrap ">
+                        <h3>Pusat Sekolah</h3>
+                        <div>
+                            <ul  class="footer-contacts no-list-style">
+                                <li><a href="tampilan-job.html" target="_blank">Job Pusat Sekolah</a></li>
+                                <li><a href="tampilan-nilaips-user.html" target="_blank">Nilai Kami !</a></li>
+                                <li><a href="tampilan-pusatbantuan.html" target="_blank">Layanan Bantuan</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- footer-widget end-->
+            </div>
+        </div>
+        <!-- footer bg-->
+        <div class="footer-bg" data-ran="4"></div>
+        <div class="footer-wave">
+            <svg viewbox="0 0 100 25">
+                <path fill="#fff" d="M0 30 V12 Q30 17 55 12 T100 11 V30z" />
+            </svg>
+        </div>
+        <!-- footer bg  end-->
+    </div>
+    <!--footer-inner end -->
+    <!--sub-footer-->
+    <div class="sub-footer  fl-wrap">
+        <div class="container">
+            <div style="align-items: center; color: #ffffff"> &#169; Pusat Sekolah 2021 .  All rights reserved.</div>
+        </div>
+    </div>
+    <!--sub-footer end -->
+</footer>
+<!--footer end --> 
