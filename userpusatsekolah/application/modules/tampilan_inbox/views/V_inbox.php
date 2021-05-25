b98abbb5e260e283f474feca82b5b87f50628675
Dhinda_Mak <dhiindarj311@gmail.com>
2021-05-25 09:48:17 +0700

    Update href

--- userpusatsekolah/application/modules/tampilan_inbox/views/V_inbox.php
+++ userpusatsekolah/application/modules/tampilan_inbox/views/V_inbox.php
@@ -59,14 +59,14 @@
                         <div class="user-profile-menu">
                             <h3>AKUN SAYA</h3>
                             <ul class="no-list-style">
-                                <li><a href="tampilan-inbox-user.html" class="user-profile-act"><i class="fal fa-envelope"></i> Inbox <span>3</span></a></li>
-                                <li><a href="tampilan-favoritsaya.html"><i class="fal fa-heart"></i>Favorit Saya</a></li>
-                                <li><a href="tampilan-terakhirdilihat-user.html"><i class="fal fa-dot-circle"></i>Terakhir Dilihat <span>7</span></a></li>
-                                <li><a href="tampilan-hasilreport-user.html"><i class="fal fa-times-circle"></i> Hasil Report</a></li>
-                                <li><a href="tampilan-edit-profiluser.html"><i class="fal fa-user-edit"></i> Edit profil</a></li>
-                                <li><a href="tampilan-alumni-user.html"><i class="fal fa-layer-plus"></i>Alumni</a></li>
-                                <li><a href="tampilan-undang-teman.html"><i class="fal fa-user-plus"></i>Undang Teman</a></li>
-                                <li><a href="tampilan-ubah-password.html"><i class="fal fa-key"></i>Ubah Password</a></li>
+                                <li><a href="<?php echo base_url('tampilan_inbox'); ?>"><i class="fal fa-envelope"></i> Inbox<span>3</span></a></li>
+                                <li><a href="<?php echo base_url('tampilan_favoritsaya'); ?>"><i class="fal fa-heart"></i>Favorit Saya</a></li>
+                                <li><a href="<?php echo base_url('tampilan_terakhirdilihat'); ?>"><i class="fal fa-dot-circle"></i>Terakhir Dilihat <span>7</span></a></li>
+                                <li><a href="<?php echo base_url('tampilan_hasilreport'); ?>"><i class="fal fa-times-circle"></i> Hasil Report</a></li>
+                                <li><a href="<?php echo base_url('tampilan_akunsaya'); ?>" class="user-profile-act"><i class="fal fa-user-edit"></i> Edit profil</a></li>
+                                <li><a href="<?php echo base_url('tampilan_alumniuser'); ?>"><i class="fal fa-layer-plus"></i>Alumni</a></li>
+                                <li><a href="<?php echo base_url('tampilan_undangteman'); ?>"><i class="fal fa-user-plus"></i>Undang Teman</a></li>
+                                <li><a href="<?php echo base_url('tampilan_ubahpassword'); ?>"><i class="fal fa-key"></i>Ubah Password</a></li>
                             </ul>
                         </div>
                         <!-- user-profile-menu end-->
@@ -74,11 +74,10 @@
                         <div class="user-profile-menu">
                             <h3>BANTUAN</h3>
                             <ul class="no-list-style">
-                                <li><a href="dashboard-listing-table.html"><i class="fal fa-th-list"></i> Pusat Bantuan </a></li>
-                                <li><a href="dashboard-bookings.html"> <i class="fal fa-calendar-check"></i> Kebijakan Pusat Sekolah <span>2</span></a></li>
-                                <li><a href="tampilan-nilaips-user.html"><i class="fal fa-comments-alt"></i> Suka Pusat Sekolah? Nilai Kami! </a></li>
-                                <li><a href="tampilan-hapusakun-user.html"><i class="fal fa-edit"></i> Ajukan Penghapusan Akun</a></li>
-                                <li><a href="tampilan-chatps-user.html"><i class="fal fa-envelope"></i> Chat Dengan Pusat Sekolah <span>3</span></a></li>
+                                <li><a href="<?php echo base_url('pusat_bantuan'); ?>"><i class="fal fa-th-list"></i> Pusat Bantuan </a></li>
+                                <li><a href="<?php echo base_url('tampilan_nilaikami'); ?>"><i class="fal fa-comments-alt"></i> Suka Pusat Sekolah? Nilai Kami! </a></li>
+                                <li><a href="<?php echo base_url('tampilan_hapusakun'); ?>"><i class="fal fa-edit"></i> Ajukan Penghapusan Akun</a></li>
+                                <li><a href="<?php echo base_url('tampilan_chatps'); ?>"><i class="fal fa-envelope"></i> Chat Dengan Pusat Sekolah <span>3</span></a></li>
                             </ul>
                         </div>
                         <!-- user-profile-menu end-->
