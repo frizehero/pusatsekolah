bb54c770e56143f4c81e2b2d267777922f5edd68
farhanariyanto03 <79591327+farhanariyanto03@users.noreply.github.com>
2021-06-15 14:28:45 +0700

    Update V_data_alumni.php

--- pusatsekolah/application/modules/data_alumni/views/V_data_alumni.php
+++ pusatsekolah/application/modules/data_alumni/views/V_data_alumni.php
@@ -5,7 +5,7 @@
                 <div class="col-12 col-md-9 p-0 mb-3">
                     <div class="search-wrapper active">
                         <div class="input-holder" style="background-color: #fff;">
-                            <?php echo form_open('produk/search') ?>
+                            <?php echo form_open('data_alumni/search') ?>
                                 <input type="text" name="nama" class="search-input" placeholder="Cari...">
                                 <button class="search-icon" style="background-color: #fff;"><span></span></button>
                             <?php echo form_close() ?>
